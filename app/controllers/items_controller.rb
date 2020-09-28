7181bbe57e9cf8062a8bd24c1b3d1f392b6459cc
risanomura110 <aiueo110boy@gmail.com>
2020-09-28 15:48:41 +0900

    edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    # @items = Item.all
     @items = Item.order('created_at DESC')
   end
 
@@ -55,6 +54,24 @@ class ItemsController < ApplicationController
     @item.destroy
     redirect_to action: 'index'
   end
+
+
+
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+  redirect_to action: 'index'
+  else
+    render :edit
+  end
+end
+
+
   private
 
   def item_params
