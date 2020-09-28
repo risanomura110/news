1ae2f16223aa78d21dbe9375bc480cc57ecfb010
risanomura110 <aiueo110boy@gmail.com>
2020-09-28 10:58:31 +0900

    削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  get 'home/show'
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items, only: [:index, :new, :create, :show] do
