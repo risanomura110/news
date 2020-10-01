01022f21df72cedbd62096a0677c4127b9e40152
risanomura110 <aiueo110boy@gmail.com>
2020-10-01 19:23:53 +0900

    res

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,13 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("../jquery1.4.4.min.js")
+require("../script.js")
+require("../html5.js")
+require("../css3-mediaqueries.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
